d8697054968e35d0caf8f42f3feb2d13bc3e3941
Jarrod Merryman <jarrodmerryman@gmail.com>
2023-02-27 23:05:32 -0700

    removes unused code and adds connect controller tests

--- src/connect/controllers/connect.controller.spec.ts
+++ src/connect/controllers/connect.controller.spec.ts
@@ -1,18 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConnectController } from './connect.controller';
+import { UsersService } from '../../users/services/users.service';
+import { ConnectService } from '../services/connect.service';
+import { Request, Response } from 'express';
+import { User } from '../../users/schemas/users.schema';
+import { UsersModule } from '../../users/users.module';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 describe('ConnectController', () => {
-  let controller: ConnectController;
+  let connectController: ConnectController;
+  let connectService: ConnectService;
+  const createUserDto: CreateUserDto = {
+    id: '12345',
+    username: 'string',
+    discriminator: 'string',
+  };
+  const mockUser = {
+    username: 'string',
+    discriminator: 'string',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [],
       controllers: [ConnectController],
+      providers: [
+        ConnectService,
+        {
+          provide: UsersService,
+          useValue: {
+            findByDiscordId: jest.fn().mockResolvedValue(mockUser),
+            update: jest.fn().mockResolvedValue(mockUser),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<ConnectController>(ConnectController);
+    connectController = module.get<ConnectController>(ConnectController);
+    connectService = module.get<ConnectService>(ConnectService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('GET connect', () => {
+    it('should return user object', async () => {
+      const req = { user: { id: '1' } } as Request;
+      const res = {} as Response;
+      const result = await connectController.getConnect(req, res);
+
+      expect(result).toEqual({ user: mockUser });
+    });
+  });
+
+  describe('POST connect', () => {
+    it('should update user with encrypted ESPN S2 and SWID', async () => {
+      const encryptedEspnS2 = 'encryptedEspnS2';
+      const encryptedSwid = 'encryptedSwid';
+      const req = { user: { id: '1' } } as Request;
+      const res = {} as Response;
+      const connectDto = { espn_s2: encryptedEspnS2, swid: encryptedSwid };
+      const result = await connectController.postConnect(connectDto, req, res);
+      expect(result).toEqual({ user: mockUser });
+    });
   });
 });
--- src/shared/providers/mongoose-connection/mongoose-connection.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { mongooseConnectionProvider } from './mongoose-connection.provider';
-
-@Module({
-  providers: [...mongooseConnectionProvider],
-  exports: [...mongooseConnectionProvider],
-})
-export class MongooseConnectionModule {}
