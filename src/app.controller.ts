3db5424e8605790248c18757bfaa753ae381e709
Jarrod Merryman <jarrodmerryman@gmail.com>
2024-04-30 00:02:54 -0600

    updates base endpoint to redirect to connect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,8 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Redirect()
+  redirectToConnect() {
+    return {
+      url: '/connect',
+      statusCode: 302,
+    };
   }
 
   @Get('livez')
